6a42056bdd54075238c1c712dec1b18c8f23e1ea
cxy <cxy0036@gmail.com>
2018-05-16 21:07:57 +0800

    no

--- PL/probe project/User/skin_color_check.c
+++ PL/probe project/User/skin_color_check.c
@@ -7,19 +7,12 @@ uint16_t readdata;
 //uint16_t readdataR;
 //uint16_t readdataG;
 //uint16_t readdataB;
-extern uint8_t ADD ,DEC,add_,dec_;
-extern void Uart_SendByte(uint8_t Data);
 
 void Skin_Color_Check(void)
 {
 	readdata = ADC_Read_Channel_Fun(ADC_RGB_R_CHANNEL);
-	Uart_SendByte(readdata);
-	Uart_SendByte(0x99);
 	//readdataG = ADC_Read_Channel_Fun(ADC_RGB_G_CHANNEL);
-	if(DEC){readdata = readdata - readdata_1V*dec_;DEC = 0;}
-	if(ADD){readdata = readdata + readdata_1V*add_;ADD = 0;}
-	Uart_SendByte(readdata);
-	Uart_SendByte(0x66);
+	//readdata = readdataG - readdataR;
 	if((readdata < COLOR_TPYE_1_H) && (readdata > COLOR_TPYE_1_L))
 	{	 
 			Skin_Tpye = 1;		
