77c2518d29a66e7ed10d3582e8ff4e1a01cfb80b
cxy <cxy0036@gmail.com>
2018-05-28 19:14:48 +0800

    5-28

--- PL/PL3000_STM8/USER/flash_times.h
+++
@@ -1,9 +0,0 @@
-#ifndef __FLASH_TIMES_H
-#define __FLASH_TIMES_H
-
-void Get_Flash_Times(void);
-void Get_Flash_Times_Level(void);
-void Get_Skin_Color(void);
-//void Flash_Times_LED_Indicator(void);
-
-#endif
